3d446ec40ca56a4797b95c0bb810d3ed42771e72
deepashre <86648444+deepashre@users.noreply.github.com>
2021-08-11 13:00:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ resource "aws_instance" "test" {
     ami = "ami-04cedf5afbdca0e15"
     instance_type = "t2.micro"
     key_name = "cloud-poc"
+    associate_public_ip_address = "true"
     
     tags = {
     Name = "ami-test"
