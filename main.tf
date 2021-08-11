1b0afbc002986e2218cc1d0af955785ebdcbf43c
deepashre <86648444+deepashre@users.noreply.github.com>
2021-08-11 12:54:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = var.aws_region
 }
-resource "aws_instance" "ami-test" { 
-    ami = " ami-04cedf5afbdca0e15"
+resource "aws_instance" "test" { 
+    ami = "ami-04cedf5afbdca0e15"
     instance_type = "t2.micro"
     key_name = "cloud-poc"
     
@@ -12,5 +12,5 @@ resource "aws_instance" "ami-test" {
 
 }
 output "publicip" {
-  value = aws_instance.ami-test.public_ip
+  value = aws_instance.test.public_ip
 }
